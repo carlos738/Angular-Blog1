addeba42dd442218a429f98532d27df618cdd8ac
carlos738 <carlosasl135@hotmail.com>
2022-11-29 21:03:10 -0300

    dinamizando as páginas

--- Angular-Blog1/src/app/data/dataFake.ts
+++ Angular-Blog1/src/app/data/dataFake.ts
@@ -11,5 +11,18 @@ export const dataFake =
     "title":"Liga da justiça",
     "description":"SUPERMAN O HOMEM DE AÇO.",
     "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F62%2F16%2F76%2F62167657822a2c12936c5fef4a428af1.jpg&f=1&nofb=1&ipt=1e4657c4288f96577156cfd4a0085d18ebb01aabed09272af99e6d6b65c92c51&ipo=images"
-  }
+  },
+  {
+    "id": "3",
+    "title":"Liga da justiça",
+    "description":"NOVO FILME DA MULHER-GAVIÃO",
+    "photoCover":"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Faa%2F98%2F10%2Faa98102fa040f6115d8336669a352a05.jpg&f=1&nofb=1&ipt=4c8aa5dc56969130423b079fa909fd9d56c5f04725b96525b7f6993d66a87b1d&ipo=images"
+  },
+  {
+    "id": "4",
+    "title":"Liga da justiça",
+    "description":"NOVO FILME DO LANTERNA VERDE",
+    "photoCover":"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F2.bp.blogspot.com%2F-KB8zpuqItS0%2FVT5ZqeiRaoI%2FAAAAAAAAFCs%2FAEDy6mqmVys%2Fs1600%2F5%252BAtores%252Bpara%252Bviver%252Bo%252BLanterna%252BVerde%252Bnos%252Bcinemas.jpg&f=1&nofb=1&ipt=577d0e9a20a5aec2082835aee7155bf4a6a97d1f0df85bffc41be763aa7f35ed&ipo=images"
+  },
+  
 ]
